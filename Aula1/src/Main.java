9f8773a4d8e02c53f40c1f776f10d387187c4a8e
MurilloZuffo <murillo.zuffo@gmail.com>
2022-05-16 22:48:33 -0300

    Aula_1

---
+++ Aula1/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 100;
+        short s1 = 16522;
+        int i1 = 520;
+        long l1 = 550;
+        float f1 = 12.55f;
+        double d1 = 12.55;
+        char c1 = 'M';
+        boolean t1 = true;
+
+
+        System.out.println("Hello world! Primeiro programa no CTD em Java!");
+        System.out.println("meu Numero Byte = " + b1 + " meu numero short é " +s1);
+        System.out.println("meu Numero Int = " + i1 + " meu numero long é " +l1);
+        System.out.println("meu Numero float = " + f1 + " meu numero double é " +d1);
+        System.out.println("minha letra Char = " + c1 + " meu boolean é " +t1);
+    }
+
+}
